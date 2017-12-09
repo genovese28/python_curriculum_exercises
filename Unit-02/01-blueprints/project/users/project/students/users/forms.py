24c338162f34f88fe61eba6646b3c464f82d3836
michellecarey <genovese28@yahoo.com>
2017-12-09 11:29:59 -0800

    updates

--- Unit-02/01-blueprints/project/users/project/students/users/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, validators
-
-class MessageForm(FlaskForm):
-	content = StringField('Content', [validators.DataRequired])
-
-class DeleteForm(FlaskForm):
